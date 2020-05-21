b20e43f22123544f93aa6f50b910cfa20e26fdf7
brontasaurus <bronte.kunkler@gmail.com>
2020-05-20 20:00:11 -0700

    i need to do #3 of nesting 4 loops
    uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuggggggggggggggggggggggggggggggghhhhhhhhhhhhhhhhhhhhhhhhh

--- src/_02_nested_loops/_3_for_loop_gauntlet/gobletOfire.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/gobletOfire.java
@@ -9,16 +9,69 @@ public static void main(String[] args) {
 //	}
 	
 	
-	for (int inknit=100; inknit>=0; inknit--) {
-		System.out.println(inknit);
-		
+	//for (int inknit=100; inknit>=0; inknit--) {
+		//System.out.println(inknit);
+		//
+	//}
+	
+//	for (int inknit=0; inknit<101; inknit++) {
+//		if(inknit % 2==0)
+//		System.out.println(inknit);
+//		
+//	}
+	
+//	for (int inknit=0; inknit<100; inknit++) {
+//		if(inknit % 2 !=0)
+//		System.out.println(inknit);	
+//	}
+	
+//	for (int inknit=0; inknit<501; inknit++) {
+//		if(inknit % 2 ==0) {
+//		System.out.println(inknit + " is even" );	
+//		}
+//		else if(inknit % 2 !=0) {
+//			System.out.println(inknit + " is odd" );	
+//			}
+//	}
+//	for (int inknit=0; inknit<778; inknit++) {
+//		if(inknit % 7 == 0) {
+//		System.out.println(inknit);	
+//		}
+//	}
+	
+//	for (int inknit=2005; inknit<2021; inknit++) {
+//		System.out.println("in " + inknit + " I was " + (inknit-2005));	
+//		
+//	}
+	
+//	for (int inknit=0; inknit<3; inknit++) {
+//		for (int yee = 0; yee<3; yee++) {
+//			
+//		System.out.println(inknit +" " + yee);	
+//		}
+//	}
+	
+//	for (int inknit=1; inknit<10; inknit++) {
+//		//for (int yee = 0; yee<3; yee++) {
+//			if ((inknit-1) % 3 == 0) {
+//				System.out.println();
+//			}
+//		System.out.print(inknit + " ");	
+//			
+//	}
+	
+	for (int inknit=0; inknit<5; inknit++) {
+		for (int yee = 0; yee<inknit; yee++) {
+				System.out.print("* ");
+			}
+		System.out.println("* ");	
+		}	
 	}
 	
 	
 	
 	
-	
 }
 	
 	
-}
+
